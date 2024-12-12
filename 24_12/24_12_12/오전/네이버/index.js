c4cbde7964d6683b8b7b3fcdf8f82e917344caf2
sank0118 <tks9901@naver.com>
2024-12-12 17:29:40 +0900

    commit message

---
+++ 24_12/24_12_12/오전/네이버/index.js
@@ -0,0 +1,10 @@
+//실행학 싶은 동작
+function show() {
+  const modal = document.getElementById("modal");
+  modal.style = "diplay:flex";
+}
+// 끄는 동작
+function hide() {
+  const modal = document.getElementById("modal");
+  modal.style = "diplay:none";
+}
